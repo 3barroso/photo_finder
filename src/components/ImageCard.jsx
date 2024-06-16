d104cd946883235361f8425a392c407b9edadd6b
Erik <erikbarroso22@gmail.com>
2024-06-15 22:20:29 -0400

    starts addition of flickr api change, only using recent photos (not searchable)

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -8,17 +8,14 @@ export function ImageCard({ data }){
   const closeModal = () => setShowImage(false);
   const openModal = () => setShowImage(true);
 
+  const flickrImageBase = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}.jpg`;
+
   return(
     <>
       <Card className="shadow-sm bg-white rounded" onClick={openModal} >
         <Card.Body className="d-flex flex-column" >
           <Col className="d-flex flex-column align-items-center" style={{maxHeight:"320px", overflow:"scroll"}} >
-          {data.type === "video/mp4" ? (
-              <video autoPlay muted src={data.link} width="100%" height={"auto"}></video>
-            ) : (
-            <img style={{objectFit: "cover", maxHeight: "100vh"}} src={data.link} alt={data.description} width={220}/>
-            )
-          }
+          <img src={flickrImageBase}></img>
           {data.title && (data.title.length > 25 ? data.title.substring(0, 25) + "..." : data.title)}
           </Col>
         </Card.Body>
@@ -27,11 +24,7 @@ export function ImageCard({ data }){
       <Modal show={showImage} onHide={closeModal} size="xl">
         <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
-          {data.type === "video/mp4" ? (
-            <video autoPlay src={data.link} width="100%" height={"auto"}></video>
-          ) : (
-            <img src={data.link} alt={data.description} width="100%" height={"auto"} />
-          )}
+          <img src={flickrImageBase} alt={data.description} width="100%" height={"auto"} />
           { data.title && data.title }
           { data.description && <div> {data.description} </div> }
         </Modal.Body>
